2f19de23bdd72a2ee76d1a29499ca8c985cc43c6
leonardo <leonardopsantos95@gmail.com>
2025-05-26 13:15:21 -0300

    Test: add test environment configurations

--- src/test/java/com/ms/notificationservice/NotificationServiceApplicationTests.java
+++ src/test/java/com/ms/notificationservice/NotificationServiceApplicationTests.java
@@ -1,10 +1,21 @@
 package com.ms.notificationservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class NotificationServiceApplicationTests {
+
+	@MockBean
+	private RabbitTemplate rabbitTemplate;
+
+	@MockBean
+	private JavaMailSender javaMailSender;
 
 	@Test
 	void contextLoads() {
